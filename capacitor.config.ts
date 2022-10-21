5466473aa04f4b631b21eeee6b710e3930a7197a
Petrizz <pedrogonzagamendesoficial@gmail.com>
2022-10-21 02:09:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'atividade07_20202004122_20202004006',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
